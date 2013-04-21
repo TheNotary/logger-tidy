2d8432440ff00c0f9a576d535622b2064c1417d0
TheNotary <no@mail.plz>
2013-04-21 14:46:21 -0500

    slowly getting around to doing stuff.

--- lib/logger-tidy.rb
+++ lib/logger-tidy.rb
@@ -1,7 +1,20 @@
 require "logger-tidy/version"
+require 'logger'
 
+class Logger
+	def initialize(logdev, shift_age = 0, shift_size = 1048576, 
+								   tidy = false, lines_to_kill_on_tidy_routine = 0)
+	  
+	  super(*args)
+
+
+	end
+end
+
+=begin
 module Logger
   module Tidy
     # Your code goes here...
   end
 end
+=end
